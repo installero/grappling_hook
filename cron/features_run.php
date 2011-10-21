5d33538495bf046f1219cd68909a57eb86653bae
Mihail Chubar <amuhc@ya.ru>
2011-10-21 20:17:58 +0400

    features running by cron: every 5 secs failed, every 30 mins normal

--- cron/features_run.php
+++ cron/features_run.php
@@ -12,7 +12,7 @@ chdir(Config::need('base_path'));
 
 require 'include.php';
 $test_delay = 5;
-$test_delay_normal = 120;
+$test_delay_normal = 1800;
 $failed_cnt = 0;
 $max_failed_cnt = 10;
 $lockfile = '/w/ru.jnpe.ls2/data/grappling_hook/cron/features.lock';
@@ -40,14 +40,17 @@ if (time() - $last_active > $test_delay) {
 function work() {
 	global $test_delay, $test_delay_normal, $failed_cnt, $max_failed_cnt;
 	$query = 'SELECT `id` FROM `features` WHERE 
-		(`status`=' . Feature::STATUS_FAILED . ' AND `last_run`<' . time() . '-' . $test_delay . ')
+		(`status`=' . Feature::STATUS_FAILED . ' AND `last_run`<(' . (time() - $test_delay) . '))
 		OR
-		(`last_run`<' . time() . '-' . $test_delay_normal . ')';
+		(`last_run`<(' . (time() - $test_delay_normal) . '))
+		ORDER BY `last_run`';
+
 	$arr = Database::sql2array($query, 'id');
 	$features = Features::getInstance()->getByIdsLoaded(array_keys($arr));
 	foreach ($features as $feature) {
 		/* @var $feature Feature */
 		lock_active();
+		$feature->dropCache();
 		$feature->_run();
 		_log($feature->getFilePath());
 	}
