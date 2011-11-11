863d333564af916d03fa1ed951d53282f3db9132
Vadim Venediktov <install.vv@gmail.com>
2011-11-11 12:14:53 +0400

    Ench: [Features] Some running features improvments

--- static/default/js/partials/feature.js
+++ static/default/js/partials/feature.js
@@ -1,5 +1,5 @@
-var checkWaitingStatuses = function () {
-  $.merge($('.p-feature-list.paused'),$('.p-feature-list.ok')).each(function(index) {
+var checkStatuses = function (feature_class) {
+  $('.p-feature-list.'+feature_class).each(function(index) {
     var tr =  $(this);
     var tr_class = tr.attr('class').split(' ')[1];
 
@@ -10,36 +10,59 @@ var checkWaitingStatuses = function () {
     };
 
     var jqxhr = $.post(exec_url, post_params, function(data){
-      if (data && data.success){
-        updateFeature(tr,tr_class,data)
-      } else if (data && data.error){alert(data.error);}
+      if (data && data.success){updateFeature(tr,tr_class,data);}
+      else if (data && data.error){alert(data.error);}
       else{alert(default_error_message);}
     }, "json");
 
   });
 }
 
-var checkAllStatuses = function () {
-  $('.p-feature-list').each(function(index) {
-    var tr =  $(this);
-    var tr_class = tr.attr('class').split(' ')[1];
+var runFeature = function(element){
+  var tr = element.parents('tr');
+  var tr_class = tr.attr('class').split(' ')[1];
 
-    var post_params = {
-      jquery:'features_module',
-      action:'check',
-      id:tr.attr('id')
-    };
+  var post_params = {
+    jquery:'features_module',
+    action:'run',
+    id:tr.attr('id')
+  };
 
-    var jqxhr = $.post(exec_url, post_params, function(data){
-      if (data && data.success){
-        updateFeature(tr,tr_class,data)
-      } else if (data && data.error){alert(data.error);}
-      else{alert(default_error_message);}
-    }, "json");
-  });
+  element.html('<img src="static/default/img/ajax.gif" alt="loading..."/>');
+
+  var jqxhr = $.post(exec_url, post_params, function(data){
+    if (data && data.success){
+      updateFeature(tr,tr_class,data)
+    } else if (data && data.error){alert(data.error);}
+    else{alert(default_error_message);}
+  }, "json");
+
+  jqxhr.complete(function(){ element.html('→'); });
 }
 
-var deleteFeature = function (id, element) {
+var pauseFeature = function(element){
+  var tr = element.parents('tr');
+  var tr_class = tr.attr('class').split(' ')[1];
+
+  var post_params = {
+    jquery:'features_module',
+    action:'pause',
+    id:tr.attr('id')
+  };
+
+  element.html('<img src="static/default/img/ajax.gif" alt="loading..."/>');
+
+  var jqxhr = $.post(exec_url, post_params, function(data){
+    if (data && data.success){
+      updateFeature(tr,tr_class,data)
+    } else if (data && data.error){alert(data.error);}
+    else{alert(default_error_message);}
+  }, "json");
+
+  jqxhr.complete(function(){ element.html('='); });
+}
+
+var deleteFeature = function(id, element){
     var post_params = {
       jquery:'features_module',
       action:'delete',
@@ -47,9 +70,9 @@ var deleteFeature = function (id, element) {
     };
 
     $.post(exec_url, post_params, function(data){
-      if (data && data.success) { element.fadeOut(500); }
-      else if (data && data.error) { alert(data.error); }
-      else { alert(default_error_message); }
+      if(data && data.success){element.fadeOut(500);}
+      else if (data && data.error){alert(data.error);}
+      else{alert(default_error_message);}
     }, "json");
 }
 
@@ -61,9 +84,9 @@ var deleteGroup = function (id, element) {
     };
 
     $.post(exec_url, post_params, function(data){
-      if (data && data.success) { element.fadeOut(500); }
-      else if (data && data.error) { alert(data.error); }
-      else { alert(default_error_message); }
+      if(data && data.success){element.fadeOut(500);}
+      else if(data && data.error){alert(data.error);}
+      else{alert(default_error_message);}
     }, "json");
 }
 
@@ -71,57 +94,18 @@ $(function() {
   $('abbr.timeago').timeago();
 
   $('.run-feature').bind('click',function(){
-    var link = $(this);
-    var tr =  $(this).parents('tr');
-    var tr_class = tr.attr('class').split(' ')[1];
-
-    var post_params = {
-      jquery:'features_module',
-      action:'run',
-      id:tr.attr('id')
-    };
-
-    link.html('<img src="static/default/img/ajax.gif" alt="loading..."/>');
-
-    var jqxhr = $.post(exec_url, post_params, function(data){
-      if (data && data.success){
-        updateFeature(tr,tr_class,data)
-      } else if (data && data.error){alert(data.error);}
-      else{alert(default_error_message);}
-    }, "json");
-
-    jqxhr.complete(function(){ link.html('→'); });
-
+    runFeature($(this))
     return false;
   });
   
   $('.pause-feature').bind('click',function(){
-    var link = $(this);
-    var tr =  $(this).parents('tr');
-    var tr_class = tr.attr('class').split(' ')[1];
-
-    var post_params = {
-      jquery:'features_module',
-      action:'pause',
-      id:tr.attr('id')
-    };
-
-    link.html('<img src="static/default/img/ajax.gif" alt="loading..."/>');
-
-    var jqxhr = $.post(exec_url, post_params, function(data){
-      if (data && data.success){
-        updateFeature(tr,tr_class,data)
-      } else if (data && data.error){alert(data.error);}
-      else{alert(default_error_message);}
-    }, "json");
-
-    jqxhr.complete(function(){ link.html('='); });
-
+    pauseFeature($(this))
     return false;
   });
 
-  setInterval(function() {checkWaitingStatuses();}, 3000);
-  setInterval(function() {checkAllStatuses();}, 60000);
+  setInterval(function(){checkStatuses('waiting');},1000);
+  setInterval(function(){checkStatuses('failed');},3000);
+  setInterval(function(){checkStatuses('ok');},60000);
 
   $('.p-feature-list-delete').bind('click',function(){
     element = $(this).parents('tr');
