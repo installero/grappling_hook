ba466f70bc5e79ca18a7831697581322e6c8d0ce
Mihail Chubar <amuhc@ya.ru>
2011-11-25 12:26:05 +0400

    fixes: creating feature file

--- modules/write/FeaturesWriteModule.php
+++ modules/write/FeaturesWriteModule.php
@@ -26,9 +26,38 @@ class FeaturesWriteModule extends BaseWriteModule {
 		    'last_message' => '',
 		);
 		if ($data['title'])
-			Features::getInstance()->_create($data);
+			$data['id'] = Features::getInstance()->_create($data, false);
 		@ob_end_clean();
-		header('Location: ' . Config::need('www_path') . '/features');
+
+		if (1 || $data['description']) {
+			// пишем в файл
+			$f = '../features/' . Features::getInstance()->getByIdLoaded($data['id'])->getFilePath();
+			if (!file_exists($f)) {
+				@mkdir('../features/' . Features::getInstance()->getByIdLoaded($data['id'])->getFolder());
+				file_put_contents($f, $data['description']);
+				$file_modify = (int) @filemtime($f);
+				clearstatcache();
+				$query = 'UPDATE `features` SET `file_modify` = ' . $file_modify . ' WHERE `id`=' . $data['id'];
+				Database::query($query);
+			} else {
+				$file_modify = (int) @filemtime($f);
+				if ($file_modify > Request::post('file_modify')) {
+					// файл новее чем в базе 
+					$query = 'UPDATE `features` SET `file_modify` = ' . $file_modify . ' WHERE `id`=' . $data['id'];
+					Database::query($query);
+					throw new Exception(date('Y-m-d H:i:s') . ' File was modified at ' . date('Y-m-d H:i:s', $file_modify) . ', fetched version is ' . date('Y-m-d H:i:s', Request::post('file_modify')) . '. Please refresh page');
+				} else {
+					file_put_contents($f, $data['description']);
+					clearstatcache();
+					$file_modify = (int) @filemtime($f);
+					clearstatcache();
+					$query = 'UPDATE `features` SET `file_modify` = ' . $file_modify . ' WHERE `id`=' . $data['id'];
+					Database::query($query);
+				}
+			}
+		}
+
+		header('Location:' . Config::need('www_path') . '/features/' . $data['id']);
 		exit(0);
 	}
 
@@ -47,7 +76,7 @@ class FeaturesWriteModule extends BaseWriteModule {
 		if ($data['title'] && $data['id'])
 			Features::getInstance()->getByIdLoaded($data['id'])->_update($data);
 
-		if ($data['description']) {
+		if (1 || $data['description']) {
 			// пишем в файл
 			$f = '../features/' . Features::getInstance()->getByIdLoaded($data['id'])->getFilePath();
 			if (!file_exists($f)) {
